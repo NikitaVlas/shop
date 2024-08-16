886754f3f949d033e0f256ff1770032f896d095b
Nikita <nikistosov@gmail.com>
2024-08-16 17:13:31 +0200

    Header compnents
    - header
    - lang dat
    - fonts

---
+++ components/modules/Header/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container header__container">
+                <button className="btn-reset header__burger">
+
+                </button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
