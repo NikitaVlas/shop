52bda4a54b7826af426f8662295b4fa2e25adeea
Nikita <nikistosov@gmail.com>
2024-09-01 16:56:28 +0200

    feat: change lang
    - function change lang en and rus  was added

--- components/modules/Header/Menu.tsx
+++ components/modules/Header/Menu.tsx
@@ -3,6 +3,8 @@ import { useLang } from '@/hooks/useLang'
 import { useUnit } from 'effector-react/effector-react.mjs'
 import { $menuIsOpen, closeMenu } from '@/context/madals'
 import { removeOverflowHiddenFromBody } from '@/lib/utils/common'
+import { AllowedLangs } from '@/constants/lang'
+import { setLang } from '@/context/lang'
 
 const Menu = () => {
   const [showCatalogList, setShowCatalogList] = useState(false)
@@ -10,6 +12,14 @@ const Menu = () => {
   const [showContctsList, setShowContctsList] = useState(false)
   const menuIsOpen = useUnit($menuIsOpen)
   const { lang, translations} = useLang()
+
+  const handelSwitchLang = (lang: string) => {
+    setLang(lang as AllowedLangs)
+    localStorage.setItem('lang', JSON.stringify(lang))
+  }
+
+  const handleSwitchLangToRu = () => handelSwitchLang('ru')
+  const handleSwitchLangToEn = () => handelSwitchLang('en')
 
   const handelCloseMenu = () => {
     removeOverflowHiddenFromBody()
@@ -22,6 +32,24 @@ const Menu = () => {
         className={`btn-reset nav-menu__close ${menuIsOpen ? 'open' : ''}`}
         onClick={handelCloseMenu}
       />
+      <div className={`nav-menu__lang ${menuIsOpen ? 'open' : ''}`}>
+        <button
+          className={`btn-reset nav-menu__lang__btn ${
+            lang === 'ru' ? 'lang-active' : ''
+          }`}
+          onClick={handleSwitchLangToRu}
+        >
+          RU
+        </button>
+        <button
+          className={`btn-reset nav-menu__lang__btn ${
+            lang === 'en' ? 'lang-active' : ''
+          }`}
+          onClick={handleSwitchLangToEn}
+        >
+          EN
+        </button>
+      </div>
       <h1>Menu</h1>
     </nav>
   )
